727a771cce63d47f535aa8520416c03d86e31579
Rafeya Khandaker <rafeya.khandaker88@gmail.com>
2022-10-22 03:41:45 +0600

    cart built

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import "./Cart.css"
 const Cart = ({cart}) => {
+
+    let total = 0;
+    let shippingCharge = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shippingCharge = shippingCharge + product.shipping
+    }
+    const tax = total * 0.1;
+    const grandTotal = total + shippingCharge + tax;
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Total price: ${total}</p>
+            <p>Total Shipping: ${shippingCharge}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
