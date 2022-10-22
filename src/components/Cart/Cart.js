e66436c27efe8d160a1f54e1814001731b97ccdf
Rafeya Khandaker <rafeya.khandaker88@gmail.com>
2022-10-22 08:26:01 +0600

    cart issues fixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import "./Cart.css"
-const Cart = ({cart}) => {
-
+const Cart = (props) => {
+    const {cart}= props;
+    
     let total = 0;
     let shippingCharge = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
-        shippingCharge = shippingCharge + product.shipping
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shippingCharge = shippingCharge + product.shipping;
     }
-    const tax = total * 0.1;
-    const grandTotal = total + shippingCharge + tax;
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = parseFloat((total + shippingCharge + tax).toFixed(2));
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total price: ${total}</p>
             <p>Total Shipping: ${shippingCharge}</p>
-            <p>Tax: ${tax.toFixed(2)}</p>
-            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal}</h5>
         </div>
     );
 };
